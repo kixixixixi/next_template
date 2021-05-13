3da3f809c1dd59b2023dc448997ed4d4d23595b1
kixixixixi <kixixixixi@gmail.com>
2021-05-14 05:50:54 +0900

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+const IndexPage = (): React.ReactElement => {
+  return (
+    <>
+      <main>
+        <section>
+          <h1>Hello, world!</h1>
+        </section>
+      </main>
+    </>
+  )
+}
+
+export default IndexPage
